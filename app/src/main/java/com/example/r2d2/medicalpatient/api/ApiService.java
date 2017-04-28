3f87b552cfd5d44abf70cf17c918aeefa97a263a
evilZ1994 <yuexinzh@gmail.com>
2017-04-28 11:24:26 +0800

    添加依赖、登录界面

---
+++ app/src/main/java/com/example/r2d2/medicalpatient/api/ApiService.java
@@ -0,0 +1,8 @@
+package com.example.r2d2.medicalpatient.api;
+
+/**
+ * Created by R2D2 on 2017/4/28.
+ */
+
+public interface ApiService {
+}
---
+++ app/src/main/java/com/example/r2d2/medicalpatient/app/App.java
@@ -0,0 +1,11 @@
+package com.example.r2d2.medicalpatient.app;
+
+import android.app.Application;
+
+/**
+ * Created by R2D2 on 2017/4/28.
+ */
+
+public class App extends Application {
+
+}
